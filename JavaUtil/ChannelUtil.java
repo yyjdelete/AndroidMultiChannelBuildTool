ed021b4a2df0fd84645f3d72b70ebcd17353f850
SilverFox <yyjdelete@126.com>
2015-12-18 18:07:55 +0800

    自用版本, 修改内容: ---- 1. 使用固定channel文件, 在其中写入channel号而不是使用文件名表示(性能不如之前的, 但感觉习惯些, comment又要api17才直接有接口读) 2. 支持统计初次安装版本(需要访问getChannel) 3. GINGERBREAD及以上支持同版本更新时检测Channel改变 4. 更方便的修改channel前缀(代码中), 避免和引用的library存在冲突的情况出现 5. 支持UTF-8编码的中文channel

--- JavaUtil/ChannelUtil.java
+++ JavaUtil/ChannelUtil.java
@@ -1,25 +1,38 @@
 package com.czt.util;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Enumeration;
+import java.io.InputStreamReader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 public class ChannelUtil {
-	
-	private static final String CHANNEL_KEY = "cztchannel";
-	private static final String CHANNEL_VERSION_KEY = "cztchannel_version";
-	private static String mChannel;
+	//在多个地方用到时提供标识
+	private static final String PACKAGE_ID = "czt";//"myapp_"
+	//保存的安装包渠道
+	private static final String CHANNEL_LAST_KEY = PACKAGE_ID + "channel";
+	//保存的安装包版本
+	private static final String CHANNEL_LAST_VERSION_KEY = PACKAGE_ID + "channel_version";
+	//保存的安装包更新时间(SDK >= 9)
+	private static final String CHANNEL_LAST_UPDATE_TIME = PACKAGE_ID + "channel_last_update_time";
+	//初次安装渠道
+	private static final String CHANNEL_INSTALL_KEY = PACKAGE_ID + "channel_install";
+	//缓存安装包渠道
+	private static String sChannel;
+	//缓存初次安装渠道
+	private static String sInstallChannel;
 	/**
-	 * 返回市场。  如果获取失败返回""
+	 * 返回渠道号。  如果获取失败返回""
 	 * @param context
 	 * @return
 	 */
@@ -27,73 +40,101 @@ public class ChannelUtil {
 		return getChannel(context, "");
 	}
 	/**
-	 * 返回市场。  如果获取失败返回defaultChannel
+	 * 返回渠道号。  如果获取失败返回defaultChannel
 	 * @param context
 	 * @param defaultChannel
 	 * @return
 	 */
 	public static String getChannel(Context context, String defaultChannel) {
 		//内存中获取
-		if(!TextUtils.isEmpty(mChannel)){
-			return mChannel;
+		if(!TextUtils.isEmpty(sChannel)){
+			return sChannel;
 		}
 		//sp中获取
-		mChannel = getChannelBySharedPreferences(context);
-		if(!TextUtils.isEmpty(mChannel)){
-			return mChannel;
+		sChannel = getChannelBySharedPreferences(context);
+		if(!TextUtils.isEmpty(sChannel)){
+			return sChannel;
 		}
 		//从apk中获取
-		mChannel = getChannelFromApk(context, CHANNEL_KEY);
-		if(!TextUtils.isEmpty(mChannel)){
+		sChannel = getChannelFromApk(context, CHANNEL_LAST_KEY);
+		if(!TextUtils.isEmpty(sChannel)){
 			//保存sp中备用
-			saveChannelBySharedPreferences(context, mChannel);
-			return mChannel;
+			saveChannelBySharedPreferences(context, sChannel);
+			return sChannel;
 		}
 		//全部获取失败
 		return defaultChannel;
-    }
+	}
 	/**
-	 * 从apk中获取版本信息
+	 * 返回初装渠道号。  如果获取失败返回""
+	 * @param context
+	 * @return
+	 */
+	public static String getInstallChannel(Context context){
+		return getInstallChannel(context, "");
+	}
+	/**
+	 * 返回初装渠道号。  如果获取失败返回defaultChannel
+	 * @param context
+	 * @param defaultChannel
+	 * @return
+	 */
+	public static String getInstallChannel(Context context, String defaultChannel) {
+		//内存中获取
+		if(!TextUtils.isEmpty(sInstallChannel)){
+			return sInstallChannel;
+		}
+		//sp中获取
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+		sInstallChannel = sp.getString(CHANNEL_INSTALL_KEY, "");
+		if(!TextUtils.isEmpty(sInstallChannel)){
+			return sInstallChannel;
+		}
+		//全部获取失败
+		sInstallChannel = getChannel(context, defaultChannel);
+		return sInstallChannel;
+	}
+	/**
+	 * 从apk中获取渠道号
 	 * @param context
 	 * @param channelKey
 	 * @return
 	 */
 	private static String getChannelFromApk(Context context, String channelKey) {
 		//从apk包中获取
-        ApplicationInfo appinfo = context.getApplicationInfo();
-        String sourceDir = appinfo.sourceDir;
-        //默认放在meta-inf/里， 所以需要再拼接一下
-        String key = "META-INF/" + channelKey;
-        String ret = "";
-        ZipFile zipfile = null;
-        try {
-            zipfile = new ZipFile(sourceDir);
-            Enumeration<?> entries = zipfile.entries();
-            while (entries.hasMoreElements()) {
-                ZipEntry entry = ((ZipEntry) entries.nextElement());
-                String entryName = entry.getName();
-                if (entryName.startsWith(key)) {
-                    ret = entryName;
-                    break;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (zipfile != null) {
-                try {
-                    zipfile.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        String[] split = ret.split("_");
-        String channel = "";
-        if (split != null && split.length >= 2) {
-        	channel = ret.substring(split[0].length() + 1);
-        }
-        return channel;
+		ApplicationInfo appinfo = context.getApplicationInfo();
+		String sourceDir = appinfo.sourceDir;
+		//默认放在META-INF/里， 所以需要再拼接一下
+		String key = "META-INF/" + channelKey;
+		String channel = "";
+		ZipFile zipfile = null;
+		BufferedReader br = null;
+		try {
+			zipfile = new ZipFile(sourceDir);
+			ZipEntry entry = zipfile.getEntry(key);
+			if (entry != null) {
+				br = new BufferedReader(new InputStreamReader(zipfile.getInputStream(entry), "UTF-8"));
+				channel = br.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (zipfile != null) {
+				try {
+					zipfile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return channel;
 	}
 	/**
 	 * 本地保存channel & 对应版本号
@@ -103,33 +144,58 @@ public class ChannelUtil {
 	private static void saveChannelBySharedPreferences(Context context, String channel){
 		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
 		Editor editor = sp.edit();
-		editor.putString(CHANNEL_KEY, channel);
-		editor.putInt(CHANNEL_VERSION_KEY, getVersionCode(context));
+		editor.putString(CHANNEL_LAST_KEY, channel);
+		editor.putInt(CHANNEL_LAST_VERSION_KEY, getVersionCode(context));
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD)
+			editor.putLong(CHANNEL_LAST_UPDATE_TIME, getLastUpdateTime(context));
+		if (TextUtils.isEmpty(sp.getString(CHANNEL_INSTALL_VERSION_KEY, "")))
+			editor.putString(CHANNEL_INSTALL_VERSION_KEY, channel);
 		editor.commit();
 	}
 	/**
-	 * 从sp中获取channel
+	 * 从sp中获取channel(NOTE: 同版本升级渠道号会缓存, 不会被识别)
 	 * @param context
 	 * @return 为空表示获取异常、sp中的值已经失效、sp中没有此值
 	 */
 	private static String getChannelBySharedPreferences(Context context){
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-		int currentVersionCode = getVersionCode(context);
-		if(currentVersionCode == -1){
-			//获取错误
-			return "";
+		if (canUseSP(context)) {
+			SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+			return sp.getString(CHANNEL_LAST_KEY, "");
 		}
-		int versionCodeSaved = sp.getInt(CHANNEL_VERSION_KEY, -1);
-		if(versionCodeSaved == -1){
-			//本地没有存储的channel对应的版本号
-			//第一次使用  或者 原先存储版本号异常
-			return "";
-		}
-		if(currentVersionCode != versionCodeSaved){
-			return "";
-		}
-		return sp.getString(CHANNEL_KEY, "");
+		return "";
 	}
+
+	private static boolean canUseSP(Context context) {
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+			//GINGERBREAD及更高版本尝试检测同VersionCode, 不同渠道间的升级
+			long lastUpdateTime = getLastUpdateTime(context);
+			if (lastUpdateTime == -1) {
+				//不匹配或之前没有正确的时间
+				return false;
+			}
+			long lastUpdateTimeSaved = sp.getInt(CHANNEL_LAST_UPDATE_TIME, -1);
+			if(lastUpdateTime != lastUpdateTimeSaved) {
+				//已
+				return false;
+			}
+
+		}
+		{
+			int currentVersionCode = getVersionCode(context);
+			if (currentVersionCode == -1) {
+				//获取错误
+				return false;
+			}
+			int versionCodeSaved = sp.getInt(CHANNEL_LAST_VERSION_KEY, -1);
+			if (currentVersionCode != versionCodeSaved) {
+				//不匹配或之前没有正确的版本号
+				return false;
+			}
+		}
+		return true;
+	}
+
 	/**
 	 * 从包信息中获取版本号
 	 * @param context
@@ -138,6 +204,21 @@ public class ChannelUtil {
 	private static int getVersionCode(Context context){
 		try{
 			return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+		}catch(NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+
+	/**
+	 * 从包信息中获取最后更新时间
+	 * @param context
+	 * @return
+	 */
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+	private static long getLastUpdateTime(Context context){
+		try{
+			return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).lastUpdateTime;
 		}catch(NameNotFoundException e) {
 			e.printStackTrace();
 		}
--- PythonTool/MultiChannelBuildTool.py
+++ PythonTool/MultiChannelBuildTool.py
@@ -4,12 +4,7 @@ import zipfile
 import shutil
 import os
 
-# 空文件 便于写入此空文件到apk包中作为channel文件
-src_empty_file = 'info/czt.txt'
-# 创建一个空文件（不存在则创建）
-f = open(src_empty_file, 'w') 
-f.close()
-
+pkg_id = "czt"
 # 获取当前目录中所有的apk源包
 src_apks = []
 # python3 : os.listdir()即可，这里使用兼容Python2的os.listdir('.')
@@ -44,7 +39,7 @@ for src_apk in src_apks:
     # 遍历渠道号并创建对应渠道号的apk文件
     for line in lines:
         # 获取当前渠道号，因为从渠道文件中获得带有\n,所有strip一下
-        target_channel = line.strip()
+        target_channel = line.strip().decode('utf-8')
         # 拼接对应渠道号的apk
         target_apk = output_dir + src_apk_name + "-" + target_channel + src_apk_extension  
         # 拷贝建立新apk
@@ -52,8 +47,8 @@ for src_apk in src_apks:
         # zip获取新建立的apk文件
         zipped = zipfile.ZipFile(target_apk, 'a', zipfile.ZIP_DEFLATED)
         # 初始化渠道信息
-        empty_channel_file = "META-INF/cztchannel_{channel}".format(channel = target_channel)
+        empty_channel_file = "META-INF/{id}channel".format(id = pkg_id)
         # 写入渠道信息
-        zipped.write(src_empty_file, empty_channel_file)
+        zipped.writestr(empty_channel_file, target_channel.encode('utf-8'))
         # 关闭zip流
         zipped.close()
